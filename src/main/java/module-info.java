2923d687902bfefad5d921da62593c230f495e0e
João Lucas <jldearaujo1@gmail.com>
2025-05-09 22:18:41 -0300

    Primeiro commit(criando o projeto)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.hotelariaanjl {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.hotelariaanjl to javafx.fxml;
+    exports com.example.hotelariaanjl;
+}
