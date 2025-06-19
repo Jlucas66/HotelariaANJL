5e7e4c1c3b4e7517e2b6d12057cc4cd7b8f6a491
João Lucas <jldearaujo1@gmail.com>
2025-06-18 21:04:38 -0300

    atualização dos repositórios para Singleton.

--- src/main/java/com/example/repositories/FuncionarioRepository.java
+++ src/main/java/com/example/repositories/FuncionarioRepository.java
@@ -1,31 +1,94 @@
 package com.example.repositories;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.example.models.Funcionario;
 
-public class FuncionarioRepository {
-    private List<Funcionario> funcionarios = new ArrayList<>();
+public class FuncionarioRepository implements IRepositories {
+    private static FuncionarioRepository instance;
 
-    public void adicionar(Funcionario funcionario) {
-        funcionarios.add(funcionario);
+    private Funcionario[] funcionarios;
+    int contador;
+
+    private FuncionarioRepository() {
+        contador = 0;
     }
 
-    public List<Funcionario> listar() {
+    public static FuncionarioRepository getInstance() {
+        if (instance == null) {
+            instance = new FuncionarioRepository();
+        }
+        return instance;
+    }
+
+    @Override
+    public Object[] findAll() {
+        Funcionario[] funcionarios = new Funcionario[contador]; 
+        for (int i = 0; i< contador; i++) {
+            funcionarios[i] = this.funcionarios[i];
+        }
         return funcionarios;
     }
 
-    public Funcionario buscarPorCpf(String cpf) {
-        for (Funcionario f : funcionarios) {
-            if (f.getCpf().equals(cpf)) {
-                return f;
+    @Override
+    public Object findById(int id) {
+        for (int i = 0; i < contador; i++) {
+            if (funcionarios[i].getId().equals(String.valueOf(id))) {
+                return funcionarios[i];
             }
         }
         return null;
     }
 
-    public boolean remover(String cpf) {
-        return funcionarios.removeIf(f -> f.getCpf().equals(cpf));
+    @Override
+    public void save(Object entity) {
+        if (contador >= funcionarios.length) {
+            System.out.println("O repositório está cheio!");
+            return;
+        }
+        if (entity instanceof Funcionario) {
+            Funcionario funcionario = (Funcionario) entity;
+            funcionario.setId(String.valueOf(contador + 1)); // Simplesmente incrementa o contador para o ID
+            funcionarios[contador++] = funcionario;
+            System.out.println("Funcionário salvo com ID: " + funcionario.getId());
+        } else {
+            System.out.println("Objeto inválido, não é um Funcionario.");
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        int indexToRemove = -1;
+        for (int i = 0; i < contador; i++) {
+            if (funcionarios[i].getId().equals(String.valueOf(id))) {
+                indexToRemove = i;
+                break;
+            }
+        }
+        if (indexToRemove != -1) {
+            for (int i = indexToRemove; i < contador - 1; i++) {
+                funcionarios[i] = funcionarios[i + 1];
+            }
+            funcionarios[contador - 1] = null;
+            contador--;
+            System.out.println("Funcionário com ID " + id + " removido.");
+        } else {
+            System.out.println("Funcionário com ID " + id + " não encontrado.");
+        }
+    }
+
+    @Override
+    public void update(Object entity) {
+        if (entity instanceof Funcionario) {
+            Funcionario funcionario = (Funcionario) entity;
+            for (int i = 0; i < contador; i++) {
+                if (funcionarios[i].getId().equals(funcionario.getId())) {
+                    funcionarios[i] = funcionario;
+                    System.out.println("Funcionário com ID " + funcionario.getId() + " atualizado.");
+                    return;
+                }
+            }
+            System.out.println("Funcionário com ID " + funcionario.getId() + " não encontrado.");
+        } else {
+            System.out.println("Objeto inválido, não é um Funcionario.");
+        }
     }
 }
--- src/main/java/com/example/repositories/HospedeRepository.java
+++ src/main/java/com/example/repositories/HospedeRepository.java
@@ -1,35 +1,95 @@
 package com.example.repositories;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.example.models.Hospede;
 
-public class HospedeRepository {
-    private List<Hospede> hospedes = new ArrayList<>();
+public class HospedeRepository implements IRepositories {
+    private static HospedeRepository instance;
 
-    public void adicionar(Hospede hospede) {
-        hospedes.add(hospede);
+    private Hospede[] hospedes;
+    private int contador;
+
+    private HospedeRepository() {
+        hospedes = new Hospede[100]; // Tamanho fixo inicial
+        contador = 0;
     }
 
-    public List<Hospede> listar() {
-        return hospedes;
+    public static HospedeRepository getInstance() {
+        if (instance == null) {
+            instance = new HospedeRepository();
+        }
+        return instance;
     }
 
-    public boolean remover(String cpf) {
-        return hospedes.removeIf(h -> h.getCpf().equals(h.getCpf()));
+    @Override
+    public Object[] findAll() {
+        Hospede[] lista = new Hospede[contador];
+        for (int i = 0; i < contador; i++) {
+            lista[i] = hospedes[i];
+        }
+        return lista;
     }
 
-    public Hospede buscarHospedePorCpf(String cpf) {
-        for (Hospede h : hospedes) {
-            if (h.getCpf().equals(cpf)) {
-                return h;
+    @Override
+    public Object findById(int id) {
+        for (int i = 0; i < contador; i++) {
+            if (hospedes[i].getId().equals(String.valueOf(id))) {
+                return hospedes[i];
             }
         }
         return null;
     }
 
-    //CRUD básico feito, caso nessecite de alguma outra funcionalidade, inserir depois.
-    
+    @Override
+    public void save(Object entity) {
+        if (contador >= hospedes.length) {
+            System.out.println("O repositório está cheio!");
+            return;
+        }
+        if (entity instanceof Hospede) {
+            Hospede hospede = (Hospede) entity;
+            hospede.setId(String.valueOf(contador + 1));
+            hospedes[contador++] = hospede;
+            System.out.println("Hóspede salvo com ID: " + hospede.getId());
+        } else {
+            System.out.println("Objeto inválido, não é um Hóspede.");
+        }
+    }
 
+    @Override
+    public void delete(int id) {
+        int indexToRemove = -1;
+        for (int i = 0; i < contador; i++) {
+            if (hospedes[i].getId().equals(String.valueOf(id))) {
+                indexToRemove = i;
+                break;
+            }
+        }
+        if (indexToRemove != -1) {
+            for (int i = indexToRemove; i < contador - 1; i++) {
+                hospedes[i] = hospedes[i + 1];
+            }
+            hospedes[contador - 1] = null;
+            contador--;
+            System.out.println("Hóspede com ID " + id + " removido.");
+        } else {
+            System.out.println("Hóspede com ID " + id + " não encontrado.");
+        }
+    }
+
+    @Override
+    public void update(Object entity) {
+        if (entity instanceof Hospede) {
+            Hospede hospede = (Hospede) entity;
+            for (int i = 0; i < contador; i++) {
+                if (hospedes[i].getId().equals(hospede.getId())) {
+                    hospedes[i] = hospede;
+                    System.out.println("Hóspede com ID " + hospede.getId() + " atualizado.");
+                    return;
+                }
+            }
+            System.out.println("Hóspede com ID " + hospede.getId() + " não encontrado.");
+        } else {
+            System.out.println("Objeto inválido, não é um Hóspede.");
+        }
+    }
 }
